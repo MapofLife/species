3c35dc9cd49773d65da990b8ac83a05dae9e2d2d
Jeremy Malczyk <jmalczyk@gmail.com>
2016-05-05 11:35:08 -0400

    Splitting into src and dist folders for dev and production.

---
+++ Gruntfile.js
@@ -0,0 +1,54 @@
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    dom_munger: {
+      main: {
+        options: {
+            read: [
+              {selector:'link.dev',attribute:'href',writeto:'cssRefs',isPath:true},
+              {selector:'script.dev',attribute:'src',writeto:'jsRefs',isPath:true}
+            ],
+            remove: ['.dev'],
+            append: [
+              {selector:'head',html:'<link href="static/app.min.css" rel="stylesheet">'},
+              {selector:'head',html:'<script src="static/app.min.js"></script>'}
+            ]
+        },
+        src: 'src/index.html',
+        dest: 'dist/index.html'
+      },
+    },
+    cssmin: {
+      main: {
+        src:'<%= dom_munger.data.cssRefs %>', //use our read css references and concat+min them
+        dest:'dist/static/app.min.css'
+      }
+    },
+    uglify: {
+      options: {mangle: false},
+      main: {
+        src: '<%= dom_munger.data.jsRefs %>', //use our read js references and concat+min them
+        dest:'dist/static/app.min.js'
+      }
+    },
+    copy : {
+      main: {
+   files: [
+     // includes files within path
+     { expand:true, nonull: true, cwd: 'src', src: ['static/app/**/*.html'], dest: 'dist/'}
+   ],
+ },
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-dom-munger');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task(s).
+  grunt.registerTask('default', ['dom_munger','uglify','cssmin','copy']);
+
+};
