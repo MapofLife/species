ccb50a42c8ed4f40a10eb505b500e3df1089ed3c
Jeremy Malczyk <jmalczyk@gmail.com>
2016-05-20 15:13:47 -0400

    Making it work on gh-pages

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
-
+var rewrite = require('connect-modrewrite');
 module.exports = function(grunt) {
 
   // Project configuration.
@@ -36,19 +36,65 @@ module.exports = function(grunt) {
     },
     copy : {
       main: {
-   files: [
-     // includes files within path
-     { expand:true, nonull: true, cwd: 'src', src: ['static/app/**/*.{html,png,jpg}'], dest: 'dist/'}
-   ],
- },
+        files: [
+          // includes files within path
+          { expand:true, nonull: true, cwd: 'src', src: ['static/app/**/*.{html,png,jpg}'], dest: 'dist/'}
+        ],
+      },
+    },
+    connect: {
+      server: {
+        options: {
+          port: 9001,
+          base: '',
+          keepalive: true,
+          // http://danburzo.ro/grunt/chapters/server/
+          middleware: function(connect, options, middlewares) {
+            // 1. mod-rewrite behavior
+            var rules = [
+                '!\\.html|\\.js|\\.css|\\.svg|\\.jp(e?)g|\\.png|\\.gif$ /index.html'
+            ];
+            middlewares.unshift(rewrite(rules));
+            return middlewares;
+          }
+        },
+        proxies: [
+               {
+                   context: '/species',
+                   host: 'localhost',
+                   port: 9001
+               }
+           ]
+      }
+    },
+    watch: {
+      scripts: {
+        files: ['src/*'],
+        tasks: ['dom_munger','uglify','cssmin','copy'],
+        options: {
+          spawn: false,
+        },
+      },
     }
   });
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-dom-munger');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-connect-proxy');
+  grunt.loadNpmTasks('grunt-connect-rewrite');
 
   // Default task(s).
   grunt.registerTask('default', ['dom_munger','uglify','cssmin','copy']);
+
+  grunt.registerTask('server', function (target) {
+    grunt.task.run([
+        'configureProxies:server',
+        'connect:server',
+        'watch'
+    ]);
+});
 
 };
