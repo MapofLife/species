669d321d75699d12cdadac7e20cf7b1d170b9c4a
Jeremy Malczyk <jmalczyk@gmail.com>
2016-06-24 10:26:47 -0400

    i18n support for routing

--- src/static/app/js/app.js
+++ src/static/app/js/app.js
@@ -29,10 +29,52 @@ angular.module('mol', [
   'km2',
   'angular-loading-bar',
 ])
+.config(["$httpProvider", function($httpProvider) {
+    $httpProvider.interceptors.push('middleware');
+}])
+.factory('middleware', function() {
+    return {
+        request: function(config) {
+            // need more controlling when there is more than 1 domain involved
+
+            config.url = (config.url.indexOf('/')!=0 &&
+              config.url.indexOf('http')!=0) ?
+                "//localhost:9001/species/" + config.url :
+                config.url;
+            return config;
+        }
+    };
+})
+.config(function($stateProvider) {
+  $stateProvider.decorator('url', function (state, parent) {
+  var result = {},
+      views = parent(state);
+
+  angular.forEach(views, function (config, name) {
+    var autoName = (state.name + '.' + name).replace('.', '/');
+    config.templateUrl = config.templateUrl || '/partials/' + autoName + '.html';
+    result[name] = config;
+  });
+  return result;
+  });
+  $stateProvider.decorator('views', function (state, parent) {
+  var result = {},
+      views = parent(state);
+
+  angular.forEach(views, function (config, name) {
+    var autoName = (state.name + '.' + name).replace('.', '/');
+    config.templateUrl = config.templateUrl || '/partials/' + autoName + '.html';
+    result[name] = config;
+  });
+  return result;
+  });
+})
+//.constant('molLang', document.getElementById('mol-lang').value)
+//.constant('molRegion', document.getElementById('mol-region').value)
 .config(function(uiGmapGoogleMapApiProvider) {
     uiGmapGoogleMapApiProvider.configure({
-        //    key: 'your api key',
-        v: '3.20', //defaults to latest 3.X anyhow
+        key: 'AIzaSyABlkTTWW1KD6TrmFF_X6pjWrFMGgmpp9g',
+        v: '3.30', //defaults to latest 3.X anyhow
         libraries: 'weather,geometry,visualization'
     });
 })
@@ -48,10 +90,14 @@ angular.module('mol', [
     "{scientificname}?" + //taxon
     "regiontype&region&" + //region constraint
     "dsid&type&" + //selected data options
-    "embed&sidebar&header&subnav&footer&speciessearch&regionsearch", base = '';
+    "embed&sidebar&header&subnav&footer&speciessearch&regionsearch";
+
+
+
   $sceDelegateProvider.resourceUrlWhitelist([
       'self',
       'http*://localhost**',
+      'http*://127.0.0.1:9001/**',
       'http*://*mol.org/**',
       'http*://api.mol.org/**',
       'http*://mapoflife.github.io/**'
@@ -59,18 +105,13 @@ angular.module('mol', [
   $httpProvider.defaults.useXDomain = true;
   //send cookies
   $httpProvider.defaults.withCredentials = false;
-  $urlRouterProvider.otherwise('/');
+  $urlRouterProvider.otherwise("/");
 
   $stateProvider
     .state(
       'species', //this view contains the bones of the Species Info pages (name, pic, & search bar)
       {
         abstract: true,
-      /*  resolve: {
-          data: function (molMeta) {
-            return molMeta();
-          }
-        },*/
         views: {
           "": {
             templateUrl: 'static/app/layouts/base-static.html',
@@ -79,7 +120,6 @@ angular.module('mol', [
             templateUrl: 'static/app/layouts/map-with-sidebars.html'
           }
         }
-
       }
     )
     .state(
@@ -89,7 +129,7 @@ angular.module('mol', [
           "@" :{templateUrl: "static/app/layouts/base-scrolling.html"},
           "@pa" : {templateUrl: "static/app/views/species-in-reserves/main.html"}
         },
-        url: '{0}/pa'.format(base)
+        url: '/pa'
       }
     )
     .state(
@@ -101,7 +141,7 @@ angular.module('mol', [
             controller: 'molOverviewCtrl'
           }
         },
-        url: '{0}/{1}'.format(base,params)
+        url: '/{0}'.format(params)
       }
     )
     .state(
@@ -113,7 +153,7 @@ angular.module('mol', [
             controller: 'molDetailMapCtrl'
           }
         },
-        url: '{0}/map/{1}'.format(base,params)
+        url: '/map/{0}'.format(params)
       }
     )
     .state(
@@ -125,7 +165,7 @@ angular.module('mol', [
             controller: 'molHabitatDistributionCtrl'
           }
         },
-        url: '{0}/range/{1}'.format(base,params)
+        url: '/range/{0}'.format(params)
       }
     )
     .state(
@@ -137,10 +177,14 @@ angular.module('mol', [
             controller: 'molReserveCoverageCtrl'
           }
         },
-        url: '^{0}/protect/{0}'.format(base,params)
+        url: '^/protect/{0}'.format(params)
       }
     );
     //Gets rid of the # in the querystring. Wont work on IE
-    $locationProvider.html5Mode(true);
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+    });
 
 });
+console.log(angular.module('mol'))
