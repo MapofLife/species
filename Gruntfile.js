8897942ff615c55b501ba0a24403e9a0613a65fa
Jeremy Malczyk <jmalczyk@gmail.com>
2016-07-12 16:53:26 -0400

    Fixing build

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,8 +16,8 @@ module.exports = function(grunt) {
             remove: ['.dev'],
             append: [
               {selector:'head',html:'<base href="https://mapoflife.github.io/' + pkg.base + '/" />'},
-              {selector:'head',html:'<link href="https://mapoflife.github.io/static/app.min.css" rel="stylesheet">'},
-              {selector:'head',html:'<script src="https://mapoflife.github.io/static/app.min.js"></script>'},
+              {selector:'head',html:'<link href="https://mapoflife.github.io/' + pkg.base + '/static/app.min.css" rel="stylesheet">'},
+              {selector:'head',html:'<script src="https://mapoflife.github.io/' + pkg.base + '/static/app.min.js"></script>'},
             ]
         },
         src: 'src/index.html',
