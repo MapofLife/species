727da51614b36ea0032a6dbbc6c05266d7b7d934
Jeremy Malczyk <jmalczyk@gmail.com>
2016-07-13 11:03:42 -0400

    Updating build

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,7 +15,7 @@ module.exports = function(grunt) {
             ],
             remove: ['.dev'],
             append: [
-              {selector:'head',html:'<base href="/' + pkg.base + '/" />'},
+              {selector:'head',html:'<base href="https://mapoflife.github.io/' + pkg.base + '/" />'},
               {selector:'head',html:'<meta id="mol-asset-base" content="https://mapoflife.github.io/' + pkg.base + '/" />'},
               {selector:'head',html:'<link href="https://mapoflife.github.io/' + pkg.base + '/static/app.min.css" rel="stylesheet">'},
               {selector:'head',html:'<script src="https://mapoflife.github.io/' + pkg.base + '/static/app.min.js"></script>'},
