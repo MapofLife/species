7f9b7bc07920bf97badc70106f565af60a9e8183
Jeremy Malczyk <jmalczyk@gmail.com>
2017-02-26 16:10:43 -0500

    Cleanup in overview view.

--- src/static/app/views/overview/controller.js
+++ src/static/app/views/overview/controller.js
@@ -1,70 +1,19 @@
 angular.module('mol.controllers')
   .controller('molOverviewCtrl',
   	[ '$compile',
-      '$window', '$http', '$uibModal', '$scope', '$state', '$filter', '$translate', 
+      '$window', '$http', '$uibModal', '$scope', '$state', '$filter',
       '$timeout','$location','$anchorScroll','$q','molApi','uiGmapGoogleMapApi',
    		function(
-          $compile, $window, $http, $modal, $scope, $state, $filter, $translate, 
+          $compile, $window, $http, $modal, $scope, $state, $filter,
           $timeout, $location, $anchorScroll, $q,  molApi,uiGmapGoogleMapApi) {
 
 
             $scope.selectedFamily = undefined;
-            $scope.iucnStatus = undefined;
             $scope.mapUpdater  = undefined;
-
-            //$scope.map.options.scrollwheel = false;
             $scope.map.removeOverlay(0);
-
             $scope.map.getInfoWindowModel = function(map, eventName, latLng, data) {}
 
-            function updateSpeciesDetails() {
-              
-              // Get the group
-              var selectedGroup = $scope.groups.find(function (group) {
-                return group.taxa === $scope.species.taxa
-              }) || '';
-              
-              // Get the family
-              var family = $scope.species.family;
-
-              if ($scope.species.taxonomy) {
-                var langKey = $translate.use() + "_family";
-                if ($scope.species.taxonomy[langKey]) {
-                  family = $scope.species.taxonomy[langKey];
-                }
-              }
-
-              // Set up the family display
-              $scope.selectedFamily = selectedGroup.title;
-              if (family) {
-                $scope.selectedFamily += ' | ' + family
-              }
-
-              // Set up the IUCN status
-              var iucn_status = undefined;
-              if ($scope.species.taxonomy) {
-                if ($scope.species.taxonomy.iucn_red_list_status) {
-                  var status = $scope.species.taxonomy.iucn_red_list_status;
-                  if (status == 'CR') {
-                    $scope.iucnStatus = 'Critically Endangered';
-                  } else if (status == 'EN') {
-                    $scope.iucnStatus = 'Endangered';
-                  } else if (status == 'EW') {
-                    $scope.iucnStatus = 'Extinct (Wild)';
-                  } else if (status == 'EX') {
-                    $scope.iucnStatus = 'Extinct';
-                  } else if (status == 'LC') {
-                    $scope.iucnStatus = 'Least Concern';
-                  } else if (status == 'NT') {
-                    $scope.iucnStatus = 'Near Threatened';
-                  } else if (status == 'VU') {
-                    $scope.iucnStatus = 'Vulnerable';
-                  } 
-                }
-              }
-            };
-
-              $scope.$watch(
+            $scope.$watch(
                 'species.scientificname',
                 function(name) {
                   if(name) {
@@ -74,7 +23,8 @@ angular.module('mol.controllers')
                         try{
                           $timeout.cancel($scope.mapUpdater);
                         } catch(e){}};
-                      $scope.mapUpdater = $timeout(function(){$http({
+                      $scope.mapUpdater = $timeout(function(){
+                        $http({
                       "withCredentials":false,
                       "method":"POST",
                       "timeout": $scope.canceller,
@@ -105,14 +55,22 @@ angular.module('mol.controllers')
 
                             }});},500);
 
-
-                      $timeout(500).then(function() {
-                        updateSpeciesDetails();
-                      });
-
-
                     }
                   }
                 );
 
-  }]);
+  }])
+  .filter('iucnStatus', function() {
+    return function(code) {
+      var description = {
+        'CR' : 'Critically Endangered',
+        'EN' : 'Endangered',
+        'EX' : 'Extinct',
+        'EW' : 'Extinct (Wild)',
+        'LC' : 'Least Concern',
+        'NT' : 'Near Threatened',
+        'VU' : 'Vulnerable'
+      }
+      return (code)?description[code]:undefined
+    }
+  });
