0fc02883ff2388bfe929b2a621debfe4bc6c9e11
Jeremy Malczyk <jmalczyk@gmail.com>
2017-06-05 11:05:50 -0400

    Updating build

--- src/static/app/views/overview/controller.js
+++ src/static/app/views/overview/controller.js
@@ -29,7 +29,7 @@ angular.module('mol.controllers')
                       "withCredentials":false,
                       "method":"POST",
                       "timeout": $scope.canceller,
-                      "url":"https://mol.carto.com/api/v1/map/named/consensus_map",
+                      "url":"https://carto.mol.org/user/mol/api/v1/map/named/consensus_map",
                       "data": {
                          "scientificname": name,
                       }}).success(function(result, status, headers, config) {
@@ -38,14 +38,10 @@ angular.module('mol.controllers')
                               $scope.map.setOverlay({
                                   tile_url: ""+
                                     "https://{0}/mol/api/v1/map/{1}/{z}/{x}/{y}.png"
-                                      .format(result.cdn_url.https,
-
-                                        result.layergroupid),
+                                      .format('carto.mol.org', result.layergroupid),
                                   grid_url: ""+
                                     "https://{0}/mol/api/v1/map/{1}/0/{z}/{x}/{y}.grid.json"
-                                      .format(
-                                        result.cdn_url.https,
-                                        result.layergroupid),
+                                      .format('carto.mol.org', result.layergroupid),
                                   key: result.layergroupid,
                                   attr: '©2014 Map of Life',
                                   name: 'overview',
