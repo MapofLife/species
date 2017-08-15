7fa238177ce07699d20d8030a0c45c0f26c63238
Jeremy Malczyk <jmalczyk@gmail.com>
2017-08-15 15:56:45 -0400

    Updating build

--- src/static/app/views/detailed-map/controller.js
+++ src/static/app/views/detailed-map/controller.js
@@ -52,7 +52,9 @@ angular.module('mol.controllers')
               $scope.model.canceller.resolve();
               $scope.model.canceller = $q.defer();
 
-              $scope.map.removeOverlay(0);
+              try{
+                $scope.map.removeOverlay(0);
+              } catch(e) {}
               if($scope.species) {
                   if($scope.mapUpdater) {
                     try{
